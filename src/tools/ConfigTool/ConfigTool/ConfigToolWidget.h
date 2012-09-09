e38b91fbcb69ba175bd5d77ea09a06f8760eb97c
dfighter1985 <dfighter1985.arcemu@gmail.com>
2012-09-09 18:39:22 +0200

    ADDED: #239 Initial commit of the Arcemu configuration tool.

---
+++ src/tools/ConfigTool/ConfigTool/ConfigToolWidget.h
@@ -0,0 +1,39 @@
+/*
+ * ArcEmu MMORPG Server
+ * Copyright (C) 2005-2007 Ascent Team <http://www.ascentemu.com/>
+ * Copyright (C) 2008-2012 <http://www.ArcEmu.org/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef CONFIGTOOL_H
+#define CONFIGTOOL_H
+
+#include <QtGui/QWidget>
+#include "ui_ConfigToolWidget.h"
+
+class ConfigToolWidget : public QWidget, public Ui::ConfigToolWidget
+{
+	Q_OBJECT
+
+public:
+	ConfigToolWidget( QWidget *parent = NULL );
+	~ConfigToolWidget();
+
+private Q_SLOTS:
+	void onClickOK();
+};
+
+#endif
